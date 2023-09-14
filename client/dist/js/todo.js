6005d99f888b8a964e701c54c66bb0d0feb7cdda
Jaesung Park <richdad6208@gmail.com>
2023-09-14 09:21:37 +0900

    검색기능 구현중

--- client/dist/js/todo.js
+++ client/dist/js/todo.js
@@ -1,12 +1,26 @@
 //깨달은 점 1. 코드 정리하기 전 깃커밋을 하자. 정리할 때는 고장이 잘나기 때문이다.
 
+function handleSearchTodos() {
+  debugger;
+  let searchKeyword = $searchTodosInput.value;
+  let filteredTodos = [];
+  if (searchKeyword) {
+    [...$todos.children].forEach((todo) => todo.classList.remove("active"));
+    filteredTodos = [...$todos.children].filter((todo) =>
+      todo.firstChild.data.includes(searchKeyword)
+    );
+    filteredTodos.forEach((todo) => todo.classList.add("active"));
+  }
+  createPagenation(filteredTodos);
+}
+
 function saveTodo() {
   const addTodoInputValue = $addTodoInput.value;
   removeTodoInInput();
   return addTodoInputValue;
 }
 
-function activatePagenation() {
+function activatePagenation(e) {
   let currentPageNumber = null;
   [...$pagenation.children].forEach((page, index) => {
     page.classList.toggle("active", e.target === page);
@@ -17,15 +31,20 @@ function activatePagenation() {
   showPagenation(currentPageNumber);
 }
 
-function showPagenation(numberOfPage) {
+function showPagenation(numberOfPage, arr = [...$todos.children]) {
+  debugger;
   [...$pagenation.children].forEach((item) => item.classList.remove("active"));
   $pagenation.children[numberOfPage - 1].classList.add("active");
-  showTodos(numberOfPage);
+  showTodos(numberOfPage, arr);
 }
 
-function showTodos(numberOfPage) {
-  [...$todos.children].forEach((item) => item.classList.remove("active"));
-  let activeTodos = [...$todos.children];
+function showTodos(numberOfPage, arr = [...$todos.children]) {
+  debugger;
+
+  // [...$todos.children].forEach((item) => item.classList.remove("active"));
+  // let activeTodos = [...$todos.children];
+  arr.forEach((item) => item.classList.remove("active"));
+  let activeTodos = arr;
 
   for (
     let i = PAGE_VOLUME * (numberOfPage - 1);
@@ -35,13 +54,14 @@ function showTodos(numberOfPage) {
     activeTodos[i]?.classList.add("active");
   }
 }
-function createPagenation() {
-  let currentPageVolume = Math.ceil($todos.children.length / PAGE_VOLUME);
+function createPagenation(arr = [...$todos.children]) {
+  debugger;
+  let currentPageVolume = Math.ceil(arr.length / PAGE_VOLUME);
   $pagenation.innerHTML = "";
   for (let i = 1; i <= currentPageVolume; i++) {
     $pagenation.innerHTML += `<span class="pagenation__number">${i}</span>`;
   }
-  showPagenation(currentPageVolume);
+  showPagenation(currentPageVolume, arr);
 }
 function removeTodoInInput() {
   $addTodoInput.value = "";
@@ -59,14 +79,21 @@ function addTodoItem(inputValue) {
 </li>`;
 }
 
+const $searchTodosInput = document.querySelector(".searchTodos");
+const $searchTodosButton = document.querySelector(".searchTodosButton");
 const $addTodoButton = document.querySelector(".addTodoButton");
 const $todos = document.querySelector(".todo__list");
 const $addTodoInput = document.querySelector(".addTodoInput");
 const $formInHead = document.querySelector(".formInHead");
 const $pagenation = document.querySelector(".pagenation");
 const PAGE_VOLUME = 5;
+const filteredTodosObj = {
+  keyOfFilteredTodos: [], 
+}
 
 function main() {
+  $searchTodosButton.addEventListener("click", handleSearchTodos);
+
   showPagenation(1);
   $formInHead.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -74,7 +101,9 @@ function main() {
 
   $todos.addEventListener("click", (e) => {
     deleteTodoInList(e);
-    createPagenation();
+    $searchTodosInput.value? createPagenation()
+      createPagenation();
+  
   });
 
   $addTodoButton.addEventListener("click", (e) => {
